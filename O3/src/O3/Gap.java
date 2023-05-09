5da58897a7c90123ba39401ad124013ae696504e
maithang432017 <maithang432017@gmail.com>
2023-05-10 04:25:25 +0700

    update

--- O3/src/O3/Gap.java
+++ O3/src/O3/Gap.java
@@ -2,26 +2,21 @@ package O3;
 
 public class Gap {
 public static void main(String[] args) {
-	int[] array = { 7, 4, 1, 5, 12, 15, 1, 3, 7 };
+	int[] array = { 7, 4, 1, 5, 54, 15, 13, 3, 7 };
 	System.out.println(minimumGap(array));
 }
 public static int minimumGap(int[] arr) {
-	int gap = 0;
-	int indexI = 0, indexJ = 0;
-	int j = arr.length - 1;
-	for (int i = 0; j > i; i++, j--) {
-
-		if (arr[i] - arr[j] == 0) {
-			indexI = i;
-			indexJ = j;
-			gap = indexJ - indexI;
-		}
-		if (arr[i] - arr[j] != 0) {
-			gap++;
-		}
-		gap = indexJ - indexI;
+	int count=0;
+	int gap= arr.length;
+for(int i=0; i<arr.length; i++) {
+	for(int j=i+1; j<arr.length;j++) {
+	if(arr[i]==arr[j]) {
+		count= j-i;
+		gap=count;
 	}
-	return gap;
+	}
+		
+	}return gap;
 }
 
 }
---
+++ O3/src/O3/package-info.java
@@ -0,0 +1,1 @@
+package O3;
