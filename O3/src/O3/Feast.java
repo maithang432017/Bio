e9ab0d69614c10c00debfc7d6be985c1c98423a5
maithang432017 <maithang432017@gmail.com>
2023-05-10 05:57:09 +0700

    Feast

---
+++ O3/src/O3/Feast.java
@@ -0,0 +1,23 @@
+package O3;
+
+public class Feast {
+	public static void main(String[] args) {
+		int n = 100, c = 4, m = 4;
+		System.out.println("total candy:" + totalCandy(n, c, m));
+	}
+
+	public static int totalCandy(int n, int c, int m) {
+		int candyAmount = n / c;
+		int wrap = candyAmount;
+
+		while (wrap > m) {
+
+			int freeCandy = wrap / m;
+			candyAmount += freeCandy;
+			wrap = freeCandy + wrap % m;
+
+		}
+		return candyAmount;
+	}
+
+}
